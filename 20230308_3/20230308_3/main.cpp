1d12146fbd92b40591e510eabc61d191d6321099
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-03-08 10:56:43 +0900

    Add files via upload

---
+++ 20230308_3/20230308_3/main.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	printf("1 x 1 = 1\n");
+	printf("1 x 2 = 2\n");
+	printf("1 x 3 = 3\n");
+	printf("1 x 4 = 4\n");
+	printf("1 x 5 = 5\n");
+	printf("1 x 6 = 6\n");
+	printf("1 x 7 = 7\n");
+	printf("1 x 8 = 8\n");
+	printf("1 x 9 = 9\n");
+
+
+	return 0;
+}
