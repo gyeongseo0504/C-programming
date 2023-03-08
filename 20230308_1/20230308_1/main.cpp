ae26d4519b298cac730f5e2f923550ae4cde6e5e
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-03-08 11:02:37 +0900

    Add files via upload

---
+++ 20230308_1/20230308_1/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>  //header 파일 추가 standard 10
+
+int main(void)
+{
+
+	printf("test \n");  //\n line feed한줄 건너뛰우기
+	printf("202315023 \n");  //\n line feed한줄 건너뛰우기
+	printf("Kyeong Seo Hong \n");
+	return 0;
+}
+
+
+//ctrl+F5 는 실행
