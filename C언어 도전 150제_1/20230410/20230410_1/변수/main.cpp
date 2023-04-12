c30f39594f11272b392776cc13376d36c16b5bcf
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-04-12 22:46:18 +0900

    Add files via upload

---
+++ C언어 도전 150제_1/20230410/20230410_1/변수/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+
+void main()
+{
+
+	int x = 1;
+	int y = 2;
+	int z = x + y;
+
+	printf("%d + %d = %d\n", x, y, z);
+}
---
+++ C언어 도전 150제_1/20230410/20230410_3/20230410_3/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int x = 10;
+    int y = 5;
+    int z;
+
+	z = x - y;
+
+    printf("X - Y = ");
+    printf("%d\n", z);
+    
+}
