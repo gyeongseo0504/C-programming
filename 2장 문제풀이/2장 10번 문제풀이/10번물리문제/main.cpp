a8f9b300e6c34f137e3efcd121e6487c78ce7afd
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-04-19 10:33:02 +0900

    Add files via upload

---
+++ 2장 문제풀이/2장 10번 문제풀이/10번물리문제/main.cpp
@@ -0,0 +1,30 @@
+
+void Excersize_2_10(int solution, int answer)
+{
+
+	float a = 1.7;
+	int b = 90;
+	int c = 110;
+
+
+	printf("\n\n");
+	printf("2-10 \n");
+	printf("고속도로에서 어떤 자동차가 약 %f m/s2 의 가속도를 낼 수있다   \n", &a);
+	printf("이 비율로 %d km/h 에서 %d km/h 로 가속하는데 걸리는 시간은 얼마인가?   \n", b, c);
+
+	if (solution == SHOW)
+	{
+		printf("\n\n\n");
+		printf("=================================================================\n");
+		// your code here
+		printf("관련 공식은 Vector의 성분 분해입니다.\n");
+		printf("먼저 각 이동 거리를 x,y축으로 성분 분해를 하세요.\n");
+
+
+
+		printf("=================================================================\n");
+	}
+
+
+	printf("(b) 또 이날 헬기가 움직인 총 거리는 얼마인가 ? \n\n");
+}
