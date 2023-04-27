a1e595df77dbed32ef8ad39101703ce54db56125
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-04-27 20:26:48 +0900

    Add files via upload

---
+++ 2장 문제풀이/2장 문제풀이 10번/2장 14번 문제풀이/2장 14번 문제풀이/main.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES     // M_PI를 사용하기 위해서 추가
+#include <math.h>
+
+#define SHOW          1
+#define NOT_SHOW      0
+#define Show_Solution 1
+#define Answer        1 
+
+
+#define DEG2RAD(x)  x*M_PI/180.
+#define RAD2DEG(x)  x*180/M_PI
+
+
+
+
+void Excersize_2_14(int solution, int answer)
+{
+
+	double frist_velocity = 0.0;
+	double final_velocity = 30.0;
+	double acceleration = 3.00;
+	double distance;
+
+
+	printf("\n\n");
+	printf("2_14 \n");
+	printf("경비행기가 이륙하기 위해서는 속력이 %6.2lf m/s 에 도달해야 한다.\n\n", final_velocity);
+
+	printf("%6.2lf m/s^2 으로 일정하게 가속한다면 이륙 속도에 도달하기 위해서 얼마의 활주거리가 필요한가? \n\n", acceleration);
+
+
+
+
+
+
+
+	if (solution == SHOW)
+	{
+
+
+
+		printf("\n\n\n");
+		printf("============================  풀 이  =============================\n\n");
+
+		printf("v는 최종속도, v0는 초기속도 , a는 가속도 , Δx는 거리 이므로. \n");
+		printf("v0 즉 초기속도는 출발하기 전에 속력이므로 0입니다.  \n");
+		printf("관련 공식은 거리(Δx) = (최종속도(v) - 초기속도(v0)) / (2*가속도(a)) 입니다.\n");
+		printf("관련 공식인 거리(Δx) = (최종속도(v) - 초기속도(v0)) / (2*가속도(a)) 에 대입하면\n");
+
+		printf("이 경비행기가 %6.2lf m/s 에서 %6.2lf m/s 로 %6.2lf m/s^2 를 가속하면 이륙 속도에 도달하기 위한 활주거리가 나옵니다. \n\n", frist_velocity, final_velocity, acceleration);
+
+		printf("\n=================================================================\n");
+		printf("\n\n\n");
+
+
+	}
+
+	if (answer == SHOW)
+	{
+		printf("=========================   정 답   =============================\n\n");
+
+		distance = ((final_velocity * final_velocity) - (frist_velocity * frist_velocity ))/ (2*acceleration);
+
+		printf("관련 공식인 거리(Δx) = (최종속도(v) - 초기속도(v0)) / (2*가속도(a)) 에 \n");
+		printf("v (최종속도 값), v0 (초기속도 값) , a (가속도 값)을 대입하면\n");
+		printf("Δx = (( %6.2lf m/s  -%6.2lf m/s)) / (2  *%6.2lf m/s^2)가 됩니다. \n", final_velocity, frist_velocity, acceleration, distance);
+
+		printf("따라서 이 경비행기가 %6.2lf m/s 에서 %6.2lf m/s 로 %6.2lf m/s^2 를 가속하면 나오는 Δx(활주거리)는 \n", frist_velocity, final_velocity, acceleration);
+		printf("%6.2lf m입니다. \n\n", distance);
+
+		printf("=================================================================\n");
+		printf("\n\n\n");
+	}
+
+}
+
+int main(void)
+{
+
+	printf("=================================================================\n");
+	printf("======================= Halla University ========================\n");
+	printf("======================= Future Mobility  ========================\n");
+	printf("======================== Pysics Trainer  ========================\n");
+	printf("========================   Chapter 2     ========================\n");
+	printf("=================================================================\n");
+
+
+
+	Excersize_2_14(Show_Solution, Answer);
+
+
+}
