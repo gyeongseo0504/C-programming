b0ee670e67d6f46b47edd742952c7ca4ba56233d
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-04-29 17:41:47 +0900

    Add files via upload

--- 2장 문제풀이/2장 10번 문제풀이/2장 10번 문제풀이/main.cpp
+++ 2장 문제풀이/2장 10번 문제풀이/2장 10번 문제풀이/main.cpp
@@ -41,12 +41,13 @@ void Excersize_2_10(int solution, int answer)
 		printf("============================  풀 이  =============================\n\n");
 
 		printf("관련 공식은 등가속도운동의 공식 a=Δv/Δt 입니다 \n");
-		printf("Δv는 (최종속도(v) - 초기속도(v0)) , a는 가속도 , Δt는 시간 변화량 입니다. \n");
-		printf("Δv= (최종속도) %6.2lf km/h - (초기속도) %6.2lf km/h , %6.2lf km/h . \n", final_velocity, frist_velocity , change_of_velocity);
+		printf("a는 가속도 ,Δv는 속도 변화량 , Δt는 시간 변화량 입니다. \n");
+		printf("Δv= (최종속도(v)) %6.2lf km/h - (초기속도(v0)) %6.2lf km/h , %6.2lf km/h . \n", final_velocity, frist_velocity , change_of_velocity);
+		printf("속도 변화량을 구해주면 %6.2lf = %6.2lf km/h /Δt. \n", change_of_velocity, frist_velocity);
 		printf("문제의 해답인 시간을 구하기 위해서는 km/h 를 m/s (으)로 단위를 맞춰줘야 합니다. \n");
-		printf("Δv를 1 km = 1000 m로 *1000 을(를) 해주고 h = 3600 s 이므로 /3600 s 을(를) 해줍니다.\n");
-		printf(" %6.2lf * 1000 m / 3600 s = %6.2lf m/s 이고, \n", change_of_velocity, final_change_of_velocity);
-		printf("Δt = %6.2lf m/s / %6.2lf m/s^2 = %6.2lf s \n", final_change_of_velocity, acceleration, time);
+		printf("Δv를 1 km = 1000 m로 *1000 을(를) 해주고 h = 3600 s 이므로 *3600 s 을(를) 해줍니다.\n");
+		printf("그러므로 Δv는 %6.2lf * 1000 m / 3600 s = %6.2lf m/s 이고, \n", change_of_velocity, final_change_of_velocity);
+		printf("Δt을 구하는 공식에 값들을 대입해주면 Δt = %6.2lf m/s / %6.2lf m/s^2 = %6.2lf s 입니다. \n", final_change_of_velocity, acceleration, time);
 		//110 - 90m/s  / 1.7m/s^2 = 3.27 s
 
 		printf("\n=================================================================\n");
