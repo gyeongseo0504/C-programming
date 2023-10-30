8d7025475012996b2ba0a2625554f7f8c31a46cd
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2023-10-31 00:07:31 +0900

    Add files via upload

---
+++ pointer/pointer/main.cpp
@@ -0,0 +1,95 @@
+﻿#define _CRT_SECURE_NO_WARNINGS  
+#include<stdio.h>
+
+int ReturnPlusOne(int n)//1¹ø 
+{
+	return n + 1;
+}
+
+int main()
+{
+	int number = 3;
+	printf("%d\n", number);
+
+	number = 5;
+	printf("%d\n", number);
+
+	number = ReturnPlusOne(number);
+	printf("%d\n", number);
+}
+/*
+void ChangePlusOne(int n)//2¹ø
+{
+   n += 1;
+}
+
+int main()
+{
+   int number = 3;
+   printf("%d\n", number);
+
+   number = 5;
+   printf("%d\n", number);
+
+   ChangePlusOne(number);
+   printf("%d\n", number);
+
+}
+
+
+
+
+
+
+void ChangePlusOne(int* n)//3¹ø
+{
+   *n += 1;
+}
+int main()
+{
+   int number = 3;
+   printf("%d\n", number);
+
+   number = 5;
+   printf("%d\n", number);
+
+   ChangePlusOne(&number);
+   printf("%d\n", number);
+}
+
+
+
+
+int main() //4¹ø
+{
+   int num1 = 3;
+   int num2 = 6;
+   int* num1Pointer = &num1;
+   int* num2Pointer = &num2;
+
+   printf("%d, %d\n", num1, *num1Pointer);
+   printf("%d, %d\n", num2, *num2Pointer);
+
+}
+
+
+void ChangeDouble(int* p)//5¹ø
+{
+   *p = *p * 2;
+}
+
+
+
+int main()
+{
+   int num;
+
+   printf("¼ýÀÚ ÀÔ·Â : ");
+   scanf("%d", &num);
+
+   ChangeDouble(&num);
+
+   printf("%d\n", num);
+
+
+}*/
